ad42591d53ca5dfd020f4889053b822f001af6d7
Victoria Olanipekun <queentoria1@gmail.com>
2021-08-20 18:08:16 +0100

    intro styling done

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -6,7 +6,8 @@ export default function intro() {
       <div className='left'>
         <div className="wrapper">
           <h1>Hello 👋🏽</h1>
-          <h3>I’m Victoria Olanipekun,  known socially as The Code Champ, I’m a <strong>Software Engineer</strong> with over 4 years experience in design and marketing, I currently build products, write and speak on technical development & mental health wellness for developers.  and<span></span></h3>
+          <h3>I’m Victoria Olanipekun,  known socially as  <strong>The Code Champ</strong>, I’m a Software Engineer with over 4 years experience in design and marketing, I currently build products, write and speak on technical development & mental health wellness for developers.  and<span></span></h3>
+          <button className='button'>VIEW MY LINKEDIN</button>
         </div>
       </div>
       <div className='right'>
