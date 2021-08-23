58763070252df8e988acc42b2a07e3aabb0682a4
Victoria Olanipekun <queentoria1@gmail.com>
2021-08-23 12:52:46 +0100

    skill component started

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -3,7 +3,55 @@ import './skills.scss'
 export default function Skills() {
   return (
     <div className='skills' id='skills'>
-      
+      <h3>With experience in design, software development. Some of my skillset include:</h3>
+
+      <div className='center'>
+        <div className="imgContainer">
+          <img src={"../../Assets/python.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/django.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/node.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/bootsrap.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/react.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/CSS.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/HTML.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/sass.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/js.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/git.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/postgreSQL.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/bulma.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/github.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/mongoDB.svg"} alt=""/>
+        </div>
+        <div className="imgContainer">
+          <img src={"../../Assets/figma.svg"} alt=""/>
+        </div>
+      </div>
     </div>
   )
 }
