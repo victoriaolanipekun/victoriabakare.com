7205315d59d89537888e9065870e36bc49b0dfc5
Victoria Olanipekun <queentoria1@gmail.com>
2021-08-24 15:15:51 +0100

    styling the skills component with logos and more styling

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -6,66 +6,62 @@ export default function Skills() {
       <h3>With experience in design, software development. Some of my skillset include:</h3>
 
       <div className='tools'>
-        <div className="imgContainer">
+        <img src={"../../Assets/tech.svg"} alt=""/>
+        {/* <div className="imgContainer">
           <img src={"../../Assets/python.svg"} alt=""/>
-          <h4>Python</h4>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/django.svg"} alt=""/>
-          <h4>Django</h4>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/node.svg"} alt=""/>
-          <h4>Node.JS</h4>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/bootstrap.svg"} alt=""/>
-          <h4>Bootstrap</h4>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/react.svg"} alt=""/>
-          <h4>React.JS</h4>
         </div>
         <div className="imgContainer">
-          <img src={"../../Assets/CSS.svg"} alt=""/>
-          <h4>CSS3</h4>
+          <img src={"../../Assets/CSS3.svg"} alt=""/>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/HTML.svg"} alt=""/>
-          <h4>HTML5</h4>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/sass.svg"} alt=""/>
-          <h4>Sass</h4>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/js.svg"} alt=""/>
-          <h4>Javascript</h4>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/git.svg"} alt=""/>
-          <h4>Git</h4>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/postgreSQL.svg"} alt=""/>
-          <h4>postgreSQL</h4>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/bulma.svg"} alt=""/>
-          <h4>Bulma</h4>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/github.svg"} alt=""/>
-          <h4>Github</h4>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/mongoDB.svg"} alt=""/>
-          <h4>MongoDB</h4>
         </div>
         <div className="imgContainer">
           <img src={"../../Assets/figma.svg"} alt=""/>
-          <h4>Figma</h4>
+        </div> */}
+      </div>
+      <div className='services'>
+        <h1>My Skills & Services <br/> Span through</h1>
+        <div className='service-list'></div>
+        <div className='service-list-layer'>
+          <img src={"../../Assets/design.svg"} alt=""/>
+          <h4> test</h4>
         </div>
+        <div className='service-list-two'></div>
+        <div className='service-list-two-layer'></div>
       </div>
     </div>
   )
