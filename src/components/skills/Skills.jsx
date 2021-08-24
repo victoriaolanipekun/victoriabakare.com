a81217136952d399022d7202bb96160f9a474c2f
Victoria Olanipekun <queentoria1@gmail.com>
2021-08-24 17:15:13 +0100

    skills component styling done

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -58,10 +58,27 @@ export default function Skills() {
         <div className='service-list'></div>
         <div className='service-list-layer'>
           <img src={"../../Assets/design.svg"} alt=""/>
-          <h4> test</h4>
+          <h3>UI/UX <br/> Design</h3>
+          <h4> Figma, Adobe Suite, Survey Monkey, Google Analytics </h4>
         </div>
         <div className='service-list-two'></div>
-        <div className='service-list-two-layer'></div>
+        <div className='service-list-two-layer'>
+          <img src={"../../Assets/backend.svg"} alt=""/>
+          <h3>Backend<br/>Development</h3>
+          <h4> Node.JS, Python, MongoDB, PostgreSQL, RESTfulAPIs. </h4>
+        </div>
+        <div className='service-list-three'></div>
+        <div className='service-list-three-layer'>
+          <img src={"../../Assets/frontend.svg"} alt=""/>
+          <h3>Frontend<br/>Development</h3>
+          <h4> HTML5, CSS3, Sass, Bulma, Bootstrap, React.JS, JS. </h4>
+        </div>
+        <div className='service-list-four'></div>
+        <div className='service-list-four-layer'>
+          <img src={"../../Assets/testing.svg"} alt=""/>
+          <h3>Testing &<br/>Deployment</h3>
+          <h4> Jest, Git, Github, Insomnia, Heroku, Netlify. </h4>
+        </div>
       </div>
     </div>
   )
