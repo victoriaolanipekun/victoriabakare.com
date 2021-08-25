75f7c0ec19566c3b883c9c0f63a6ebdeceaa0126
Victoria Olanipekun <queentoria1@gmail.com>
2021-08-25 07:11:59 +0100

    hobbies component done, contact component ongoing

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,26 @@ import './contact.scss'
 export default function Contact() {
   return (
     <div className='contact' id='contact'>
+      <h1>Contact</h1>
+      <div className="imgContainer">
+        <img src={"../../Assets/twitter.svg"} alt=""/>
+        <img src={"../../Assets/tiktok.svg"} alt=""/>
+        <img src={"../../Assets/linkedin.svg"} alt=""/>
+        <img src={"../../Assets/github.svg"} alt=""/>
+        <img src={"../../Assets/hasnode.svg"} alt=""/>
+      </div>
+      <h3>Hey there, thinking of collaborating on a project with me or ask for my resume ? I’d love to hear from you!</h3>
       
+      <div className='first-name'>
+        <input className='first-name' placeholder="First-name"></input>
+      </div>
+      <div className='email'>
+        <input placeholder="Your email address"></input>
+      </div>
+      <div className='email'>
+        <input placeholder="Write your message"></input>
+      </div>
+      <button>SEND EMAIL</button>
     </div>
   )
 }
--- src/components/hobbies/Hobbies.jsx
+++ src/components/hobbies/Hobbies.jsx
@@ -3,7 +3,37 @@ import './hobbies.scss'
 export default function Hobbies() {
   return (
     <div className='hobbies' id='hobbies'>
-      
+      <h1>Check out some of the things I enjoy doing</h1>
+      <div className='hobbies-one'>
+        <div className='right'>
+          <div className="wrapper">
+            <div className="wrapper-one">
+              <h2>Writing ✍🏽</h2>
+              <h3>I currently blog on hashnode where I write on technical topics and mental wellbeing in relation to developers. Do stop by and say hello when you do!</h3>
+              <div className='button-wrapper'>
+                <button className='button'>READ MY WRITING</button>
+              </div>
+            </div>
+            <div className="line"></div>
+            <div className="wrapper-two">
+              <h2>Speaking🎙</h2>
+              <h3>Speaking is a means of inspiration for me, so  I speak on different social platform on issues related to the tech ecosystem and mental well-being.</h3>
+              <div className='button-wrapper'>
+                <button className='button'>CONTACT ME</button>
+              </div>
+          </div>
+          </div>
+
+          {/* <div className="wrapper-one">
+            <h2>Speaking🎙</h2>
+            <h3>I love writing clear and readable code while working on interesting products. I have experience building fullstack applications, having worked with various languages like Python, Django, Javascript, Node.js, React.js and more.</h3>
+            <div className='button-wrapper'>
+              <button className='button'>VIEW IT HERE</button>
+              <button className='button'>VIEW GITHUB REPO</button>
+            </div>
+          </div> */}
+        </div>
+      </div>
     </div>
   )
 }
