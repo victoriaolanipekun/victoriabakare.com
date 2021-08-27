b94d59fd6f642f2ce1ad4b321e34600db3e6f445
Victoria Olanipekun <queentoria1@gmail.com>
2021-08-27 16:51:48 +0100

    mobile responsiveness started

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -6,7 +6,7 @@ export default function intro() {
       <div className='left'>
         <div className="wrapper">
           <h1>Hello 👋🏽</h1>
-          <h3>I’m Victoria Olanipekun,  known socially as  <strong>The Code Champ</strong>, I’m a Software Engineer with over 4 years of previous experience in design and marketing, I also enjoy writing and speaking on technical development  & mental health wellness for developers.</h3>
+          <h3>I’m Victoria Olanipekun,  known socially as  <strong>The Code Champ</strong>, I’m a Software Engineer with over 4 years of previous experience in design and marketing. I also enjoy writing and speaking on technical development  & mental health wellness.</h3>
           <a href="https://www.linkedin.com/in/victoria-olanipekun/" rel="noreferrer" target="_blank">VIEW MY LINKEDIN</a>
         </div>
       </div>
