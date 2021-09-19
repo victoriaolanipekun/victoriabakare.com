9f5b7cf34ffda301fd01162be526ccba81ced312
Victoria Olanipekun <queentoria1@gmail.com>
2021-09-19 20:13:14 +0100

    portfolio section responsive styling done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,7 +12,7 @@ export default function Portfolio() {
         <div className='right'>
           <div className="wrapper">
             <h2>Hanqout😃</h2>
-            <h3>This was a solo project, a fullstack application using my own RESTful API with Python(Django) as the backend language and JavaScript(React.js) for the frontend.</h3>
+            <h3>This was a solo project, a fullstack application using my own RESTful API with Python(Django) as the backend language and JavaScript(React.js) for the frontend. A project to help developer's mental health.</h3>
             <div className='button-wrapper'>
               <a href="https://github.com/victoriaolanipekun/HANQOUT#readme" rel="noreferrer" target="_blank">VIEW IT HERE</a>
               <a href="https://github.com/victoriaolanipekun/HANQOUT#readme" rel="noreferrer" target="_blank">VIEW GITHUB REPO</a>
@@ -46,7 +46,7 @@ export default function Portfolio() {
         <div className='right'>
           <div className="wrapper">
             <h2>Drinkard🍸</h2>
-            <h3>I love writing clear and readable code while working on interesting products. I have experience building fullstack applications, having worked with various languages like Python, Django, Javascript, Node.js, React.js and more.</h3>
+            <h3>This was a pair-coded project that was built using HTML5, CSS3, React.js and also integrated a 3rd party public API. It is an application which hosts a variety of 25 cocktails.</h3>
             <div className='button-wrapper'>
               <a href="https://drinkard-app.netlify.app" rel="noreferrer" target="_blank">VIEW IT HERE</a>
               <a href="https://github.com/victoriaolanipekun/DRINKARD#readme" rel="noreferrer" target="_blank">VIEW GITHUB REPO</a>
@@ -58,13 +58,12 @@ export default function Portfolio() {
       <div className='portfolio-four'>
         <div className='left'>
           <div className="imgContainer">
-            <img src={"../../Assets/snake.svg"} alt=""/>
           </div>
         </div>
         <div className='right'>
           <div className="wrapper">
             <h2>Snake Game🐍</h2>
-            <h3>I love writing clear and readable code while working on interesting products. I have experience building fullstack applications, having worked with various languages like Python, Django, Javascript, Node.js, React.js and more.</h3>
+            <h3>This is a solo project, it is a grid-based game using HTML, CSS, and JavaScript. Snake is a game where the player maneuvers a line (that is the snake) which grows in length, with the line itself being a primary obstacle.</h3>
             <div className='button-wrapper'>
               <a href="https://victoriaolanipekun.github.io/PROJECT-1/" rel="noreferrer" target="_blank">VIEW IT HERE</a>
               <a href="https://github.com/victoriaolanipekun/PROJECT-1#readme" rel="noreferrer" target="_blank">VIEW GITHUB REPO</a>
